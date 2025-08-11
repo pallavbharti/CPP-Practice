8be38823c936da89b7734c03fd59eed33edca1e0
pallavbharti <pallavbharti8795@gmail.com>
2025-08-11 08:39:37 +0530

    Modified Max In Array

--- Recursion/MaxInArray.cpp
+++ Recursion/MaxInArray.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 using namespace std;
 
-void Max(int arr[],int n ,int idx,int maxim){
+/* void Max(int arr[],int n ,int idx,int maxim){
     if(idx==n) cout<< maxim;
 
     if (maxim<arr[idx]) maxim=arr[idx];
     Max(arr,n,idx+1,maxim);
+}
+*/
+
+int MaxInArray(int arr[],int n ,int idx){
+    if(idx==n) return INT16_MIN;
+    return max(arr[idx],MaxInArray(arr,n,idx+1));
 }
 
 
 int main(){
     int arr[] ={2,4,3,63,8,9,6,5};
     int n = sizeof(arr)/sizeof(arr[0]);
-    Max(arr,n,0,INT16_MIN);
-    
+    //Max(arr,n,0,INT16_MIN);
+    cout << "Maximum value in array: " << MaxInArray(arr,n ,0) << endl;
 }
