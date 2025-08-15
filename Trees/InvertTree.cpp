21a83446325edabb4f18a4fba422968259b59e38
pallavbharti <pallavbharti8795@gmail.com>
2025-08-15 11:46:29 +0530

    Inverted Tree / Ignore executable files

---
+++ Trees/InvertTree.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// Invert function (correct signature)
+Node* invertTree(Node* root) {
+    if (root == NULL) return NULL;
+    Node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
+
+// Print function , Preorder
+void printTree(Node* root) {
+    if (root == NULL) return;
+    cout << root->val << " ";
+    printTree(root->left);
+    printTree(root->right);
+}
+
+int main() {
+    Node* a = new Node(1);
+    Node* b = new Node(2);
+    Node* c = new Node(3);
+    Node* d = new Node(4);
+    Node* e = new Node(5);
+    Node* f = new Node(6);
+    Node* g = new Node(7);
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+
+    cout << "tree before : ";
+    printTree(a);
+    cout << endl;
+    invertTree(a);
+    cout << "Inverted tree preorder: ";
+    printTree(a);
+    cout << endl;
+}
