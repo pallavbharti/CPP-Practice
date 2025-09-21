d5e79149a6a3e244b8a9a267c74bc9f34f96232f
pallavbharti <pallavbharti8795@gmail.com>
2025-09-21 23:37:33 +0530

    Generate_BinaryString_withoutConsecutive1

---
+++ Recursion/Generate_BinaryString_withoutConsecutive1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void generate(string s,int n){
+    if(s.length()==n){
+        cout<<s<<endl;
+        return;
+    }
+    generate(s+'0',n);
+    if(s=="" || s[s.length()-1]=='0') generate(s+'1',n);
+}
+int main(){
+    int n;
+    cout<<"enter length n: ";
+    cin>>n;
+    generate("",n);
+
+}
