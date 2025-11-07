991e81a3294ba525c6f6aa3dadce1eb2d2f5c2ee
pallavbharti <pallavbharti8795@gmail.com>
2025-11-07 22:14:08 +0530

    upperbound

---
+++ Binary Search/upperbound.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n , x;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter Array Element In Sorted Order: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    cout << endl;
+    cout<<"Enter target element: ";
+    cin>>x;  cout << endl;
+    int low=0 , high=n-1;
+    bool flag = false;
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        if(arr[mid]==x){
+            cout<<x <<" lower bound of target is: "<<arr[mid+1];
+            flag=true;     break;
+        }
+        else if(arr[mid]<x) low=mid+1;
+        else high = mid-1;
+    }
+    if (flag==false) cout<<arr[low];
+    
+}
